ad66a2be3220f5afa70a720f40a0480fe7e21887
Albert Brand <abrand@xebia.com>
2016-01-26 07:59:20 +0100

    Move example from react-native-android-tablayout to separate repo

---
+++ app/Routes.js
@@ -0,0 +1,22 @@
+import SimpleTabLayout from './SimpleTabLayout';
+import IconsOnTopTabLayout from './IconsOnTopTabLayout';
+import StatefulTabLayout from './StatefulTabLayout';
+import ViewPagerTabLayout from './ViewPagerTabLayout';
+import ScrollableTabLayout from './ScrollableTabLayout';
+
+export const ROUTES = [{
+  title: 'SimpleTabLayout',
+  component: SimpleTabLayout
+}, {
+  title: 'IconsOnTopTabLayout',
+  component: IconsOnTopTabLayout
+}, {
+  title: 'StatefulTabLayout',
+  component: StatefulTabLayout
+}, {
+  title: 'ViewPagerTabLayout',
+  component: ViewPagerTabLayout
+}, {
+  title: 'ScrollableTabLayout',
+  component: ScrollableTabLayout
+}];
